cfbfd71983192d79ab463f40a1daab34854bf91e
Luis Iñigo <luisvib2003@gmail.com>
2023-01-26 10:02:42 +0100

    Conexion a Clever Cloud

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -2,10 +2,11 @@ const mysql = require("mysql")
 const {HOST,USUARIO,PASSWORD,DATABASE} = require("./config")
 
 module.exports = () =>{
-    return mysql.createConnection({    
+    return mysql.createConnection({   //Podemos usar el createPool para aplicaciones grandes
         host: HOST,
         user: USUARIO,
         password: PASSWORD,
+        PORT: 3306,
         database: DATABASE
     })
 }
